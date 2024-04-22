786df6e63731d89f295586bee901569543f204dd
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-04-22 14:21:22 +0200

    adding user_id

--- database/migration.php
+++ database/migration.php
@@ -16,30 +16,11 @@ $db->dropTables();
 echo 'All tables have been dropped' . PHP_EOL;
 
 // Create tables
-
-echo 'Creating Jiri table' . PHP_EOL;
-$create_jiri_table_sql = <<<SQL
-    create table jiris
-    (
-        id          int auto_increment
-            primary key,
-        name        varchar(255)                        not null,
-        starting_at timestamp                           not null comment 'Indicates the moment the jiri should start',
-        created_at  timestamp default CURRENT_TIMESTAMP null,
-        updated_at  timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP
-    );
-SQL;
-
-$db->exec($create_jiri_table_sql);
-echo 'Jiri table created' . PHP_EOL;
-
-// Create tables
-
 echo 'Creating User table' . PHP_EOL;
 $create_user_table_sql = <<<SQL
     create table users
     (
-        id          int auto_increment
+        id          int unsigned  auto_increment
             primary key,
         name        varchar(255),
         email varchar(255)                         not null ,
@@ -51,3 +32,25 @@ SQL;
 
 $db->exec($create_user_table_sql);
 echo 'User table created' . PHP_EOL;
+
+// Create tables
+
+
+echo 'Creating Jiri table' . PHP_EOL;
+$create_jiri_table_sql = <<<SQL
+    create table jiris
+    (
+        id          int unsigned auto_increment
+            primary key,
+        name        varchar(255)                        not null,
+        starting_at timestamp                           not null comment 'Indicates the moment the jiri should start',
+        user_id     int unsigned                        not null,
+        created_at  timestamp default CURRENT_TIMESTAMP null,
+        updated_at  timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+        foreign key(user_id) references users(id)
+    );
+SQL;
+
+$db->exec($create_jiri_table_sql);
+echo 'Jiri table created' . PHP_EOL;
+
--- database/seed.php
+++ database/seed.php
@@ -1,33 +1,12 @@
 <?php
 
-// Seed tables
-echo 'Seeding Jiri table' . PHP_EOL;
-$jiris = [
-    ['name' => 'Projets Web 2024', 'starting_at' => '2024-01-19 08:30:00'],
-    ['name' => 'Design Web 2024', 'starting_at' => '2024-06-19 08:30:00'],
-    ['name' => 'Design Web 2022', 'starting_at' => '2022-06-19 08:30:00'],
-    ['name' => 'Design Web 2021', 'starting_at' => '2021-06-19 08:30:00'],
-    ['name' => 'Projets Web 2025', 'starting_at' => '2025-01-19 08:30:00'],
-    ['name' => 'Projets Web 2026', 'starting_at' => '2026-01-19 08:30:00'],
-    ['name' => 'Projets Web 2027', 'starting_at' => '2027-01-19 08:30:00'],
-    ['name' => 'Design Web 2023', 'starting_at' => '2023-06-19 08:30:00'],
-];
-$insert_jiri_in_jiris_table_sql = 'INSERT INTO jiris (name, starting_at) VALUES (:name, :starting_at)';
-$insert_jiri_in_jiris_table_stmt = $db->prepare($insert_jiri_in_jiris_table_sql);
-foreach ($jiris as $jiri) {
-    $insert_jiri_in_jiris_table_stmt->bindValue('name', $jiri['name']);
-    $insert_jiri_in_jiris_table_stmt->bindValue('starting_at', $jiri['starting_at']);
-    $insert_jiri_in_jiris_table_stmt->execute();
-}
-$count_jiris = count($jiris);
-echo "Jiri table seeded with {$count_jiris} jiris" . PHP_EOL;
-
 /*Seeding user*/
 
 echo 'Seeding User table' . PHP_EOL;
 $password = password_hash('ch4nge_th1s', PASSWORD_DEFAULT);
 $users = [
     ['email' => 'samrequena1510@gmail.com', 'password' => $password],
+    ['email' => 'sam@hepl.be', 'password' => $password],
 ];
 $insert_user_in_users_table_sql = 'INSERT INTO users (email, password) VALUES (:email, :password)';
 $insert_user_in_users_table_stmt = $db->prepare($insert_user_in_users_table_sql);
@@ -38,3 +17,28 @@ foreach ($users as $user) {
 }
 $count_users = count($users);
 echo "User table seeded with {$count_users} users" . PHP_EOL;
+
+// Seed Jiri tables
+
+echo 'Seeding Jiri table' . PHP_EOL;
+$jiris = [
+    ['name' => 'Projets Web 2024', 'starting_at' => '2024-01-19 08:30:00', 'user_id' => 1],
+    ['name' => 'Design Web 2024', 'starting_at' => '2024-06-19 08:30:00', 'user_id' => 2],
+    ['name' => 'Design Web 2022', 'starting_at' => '2022-06-19 08:30:00', 'user_id' => 2],
+    ['name' => 'Design Web 2021', 'starting_at' => '2021-06-19 08:30:00', 'user_id' => 2],
+    ['name' => 'Projets Web 2025', 'starting_at' => '2025-01-19 08:30:00', 'user_id' => 1],
+    ['name' => 'Projets Web 2026', 'starting_at' => '2026-01-19 08:30:00', 'user_id' => 1],
+    ['name' => 'Projets Web 2027', 'starting_at' => '2027-01-19 08:30:00', 'user_id' => 1],
+    ['name' => 'Design Web 2023', 'starting_at' => '2023-06-19 08:30:00', 'user_id' => 1],
+];
+$insert_jiri_in_jiris_table_sql = 'INSERT INTO jiris (name, starting_at, user_id) VALUES (:name, :starting_at, :user_id)';
+$insert_jiri_in_jiris_table_stmt = $db->prepare($insert_jiri_in_jiris_table_sql);
+foreach ($jiris as $jiri) {
+    $insert_jiri_in_jiris_table_stmt->bindValue('name', $jiri['name']);
+    $insert_jiri_in_jiris_table_stmt->bindValue('starting_at', $jiri['starting_at']);
+    $insert_jiri_in_jiris_table_stmt->bindValue('user_id', $jiri['user_id']);
+    $insert_jiri_in_jiris_table_stmt->execute();
+}
+$count_jiris = count($jiris);
+echo "Jiri table seeded with {$count_jiris} jiris" . PHP_EOL;
+
